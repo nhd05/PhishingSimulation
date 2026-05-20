4b1106d129d50ed1c839c3808a016611d1049141
cricks-18 <connercricks@gmail.com>
2026-05-20 13:32:08 -0700

    Updated App.jsx to newest version

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,33 @@
-import { Route, Routes } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+
 import Home from "./Home";
+import TrainingModules from "./TrainingModules";
+
+import Phishing from "./Phishing";
+import PasswordSecurity from "./PasswordSecurity";
+import SocialEngineering from "./SocialEngineering";
+
 import Quiz from "./Quiz";
-import Training from "./Training";
 
 function App() {
-	return (
-		<Routes>
-			<Route path="/" element={<Home />} />
-			<Route path="/training" element={<Training />} />
-			<Route path="/quiz" element={<Quiz />} />
-		</Routes>
-	);
+  return (
+    <Routes>
+
+      <Route path="/" element={<Home />} />
+
+      {/* Modules Page */}
+      <Route path="/training" element={<TrainingModules />} />
+
+      {/* Individual Modules */}
+      <Route path="/phishing" element={<Phishing />} />
+      <Route path="/passwordsecurity" element={<PasswordSecurity />} />
+      <Route path="/socialengineering" element={<SocialEngineering />} />
+
+      {/* Quiz */}
+      <Route path="/quiz" element={<Quiz />} />
+
+    </Routes>
+  );
 }
 
 export default App;
