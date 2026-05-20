4fb002126d97f4c9606c45e26ed9318095d8eb87
sakshipa-del <sakshipa@uw.edu>
2026-05-20 11:46:43 -0700

    Added training modules and quiz navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
 import { Routes, Route } from "react-router-dom";
+
 import Home from "./Home";
-import Training from "./Training";
+import TrainingModules from "./TrainingModules";
+
+import Phishing from "./Phishing";
+import PasswordSecurity from "./PasswordSecurity";
+import SocialEngineering from "./SocialEngineering";
+
 import Quiz from "./Quiz";
 
 function App() {
   return (
     <Routes>
+
       <Route path="/" element={<Home />} />
-      <Route path="/training" element={<Training />} />
+
+      {/* Modules Page */}
+      <Route path="/training" element={<TrainingModules />} />
+
+      {/* Individual Modules */}
+      <Route path="/phishing" element={<Phishing />} />
+      <Route path="/passwordsecurity" element={<PasswordSecurity />} />
+      <Route path="/socialengineering" element={<SocialEngineering />} />
+
+      {/* Quiz */}
       <Route path="/quiz" element={<Quiz />} />
+
     </Routes>
   );
 }
