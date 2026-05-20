d0f6b9e5129ba40dc5b8e399b84d17845e49a47b
cricks-18 <roughfire03404@gmail.com>
2026-05-19 22:41:23 -0700

    added deploy script, Dockerfile, and Biome config
    Adds deploy.sh pipeline script, Dockerfile for containerization, and biome.json config. Updates eslint.config.js to ignore server folder. Fixes button type warnings in Home.jsx and Training.jsx.

---
+++ Home.jsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+
+export default function Home() {
+	return (
+		<div style={{ textAlign: "center", paddingTop: "50px" }}>
+			<h1>Welcome</h1>
+
+			<Link to="/training">
+				<button
+					type="button"
+					style={{ padding: "10px 15px", marginTop: "20px" }}
+				>
+					Go to Training
+				</button>
+			</Link>
+		</div>
+	);
+}
