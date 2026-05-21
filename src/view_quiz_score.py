2c0a80b004768b4a0c867ca12ceef44921e4c263
nhd05 <nhd6@uw.edu>
2026-05-21 00:06:28 -0700

    Update view_quiz_score.py

--- src/view_quiz_score.py
+++ src/view_quiz_score.py
@@ -1,18 +1,16 @@
 
 #Score calculation logic
 def check_answer(question, user_input, correct_answer):
+    score = 0
     for user_answer in user_input:
         print("\n" + question)
         #Check the user's answer against the correct answer
-        if user_input == correct_answer:
+        if user_answer == correct_answer:
             score += 1
-            print("\nCorrect Answer!")
-        else:
-            print("wrong answer!")
 
 #Display Score
-def display_score(total_score, total_questions):
-    return{'Results': total_score + "/" + total_questions + "are correct!"}
+def display_score(score, total_questions):
+    return{'Results': score + "/" + total_questions + "are correct!"}
 
 
 
