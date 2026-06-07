e24c735ecb1df64e716d84c9927471f5e7f19850
nihalv312 <nihalv@uw.edu>
2026-06-06 20:08:48 -0700

    Add navbar component with proper formatting

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,34 @@
 import { Route, Routes } from "react-router-dom";
 
 import Home from "./Home";
+import Navbar from "./Navbar";
 import PasswordSecurity from "./PasswordSecurity";
 import Phishing from "./Phishing";
 import Quiz from "./Quiz";
 import { Results } from "./quiz_results";
 import SocialEngineering from "./SocialEngineering";
 import TrainingModules from "./TrainingModules";
-import Navbar from "./navbar";
-
 
 function App() {
 	return (
 		<div>
 			<Navbar />
 			<Routes>
-			<Route path="/" element={<Home />} />
+				<Route path="/" element={<Home />} />
 
-			{/* Modules Page */}
-			<Route path="/training" element={<TrainingModules />} />
+				{/* Modules Page */}
+				<Route path="/training" element={<TrainingModules />} />
 
-			{/* Individual Modules */}
-			<Route path="/phishing" element={<Phishing />} />
-			<Route path="/passwordsecurity" element={<PasswordSecurity />} />
-			<Route path="/socialengineering" element={<SocialEngineering />} />
+				{/* Individual Modules */}
+				<Route path="/phishing" element={<Phishing />} />
+				<Route path="/passwordsecurity" element={<PasswordSecurity />} />
+				<Route path="/socialengineering" element={<SocialEngineering />} />
 
-			{/* Quiz */}
-			<Route path="/quiz" element={<Quiz />} />
-			<Route path="/Results" element={<Results />} />
-		</Routes>
-		
-	</div>
+				{/* Quiz */}
+				<Route path="/quiz" element={<Quiz />} />
+				<Route path="/Results" element={<Results />} />
+			</Routes>
+		</div>
 	);
 }
 
